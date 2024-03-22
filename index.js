3a9fee1c914b2936ee8a481a76b1c05197655863
CVargas2170 <cvargas2170@gmail.com>
2024-03-22 01:12:26 -0400

    CRUD project and room completed

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 var cors = require ('cors');
 const connection = require('./connection');
-const userRoute = require('./routes/userRoute')
+
+//routes
+const userRoute = require('./routes/userRoute');
+const projectRoute = require('./routes/project');
+const projectRoom = require('./routes/room');
+
 const app= express();
 
 app.use(cors());
@@ -9,5 +14,8 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 app.use('/users',userRoute);
+app.use('/project',projectRoute);
+app.use('/room',projectRoom);
+
 
 module.exports= app;
---
+++ routes/room.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const connection = require('../connection');
+const router= express.Router();
+
+
+require('dotenv').config();
+
+var auth= require('../services/authentication')
+
+router.post('/create', auth.authenticateToken,(req,res)=>{  
+    let project = req.body;
+    let user = res.locals;
+    query = "INSERT INTO project (name) value(?)";
+    connection.query(query, [project.name], (err, results)=>{
+        if(!err){
+            let projectId = results.insertId;
+
+           // return res.status(200).json({message:"room created successfully"});
+            query2 = "INSERT INTO room (project_id, host_id) VALUES(?, ?)";
+            connection.query(query2,[projectId,user.id],(err, roomResult)=>{
+                if(!err){
+                    return res.status(200).json({message:"room created successfully"});
+                }else{
+                    return res.status(500).json({message:"error to create room"});
+
+                }
+            })
+        }else{
+            return res.status(500).json({message:"error "+ err});                      
+        }
+    })
+})
+
+
+router.get('/get-rooms',auth.authenticateToken, (req,res)=>{
+    let user = res.locals;
+
+    var query = "SELECT name FROM project,room WHERE project.id = room.project_id AND room.host_id= ?";
+    connection.query(query, [user.id],(err, results)=>{
+        if(!err){
+            return res.status(200).json(results);
+        }else{
+            return res.status(500).json(err);
+        }
+    })
+})
+
+module.exports= router
