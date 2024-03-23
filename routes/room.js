8ce624aa2addd53e08b8a55729f24c0318411fae
CVargas2170 <cvargas2170@gmail.com>
2024-03-22 23:38:09 -0400

    deleted room and project completed

--- routes/room.js
+++ routes/room.js
@@ -31,11 +31,36 @@ router.post('/create', auth.authenticateToken,(req,res)=>{
     })
 })
 
+router.delete('/delete-room/:id', auth.authenticateToken,(req,res)=>{
+    let id = req.params.id;
+    let room;
+    getRoom(id, (error, registro) => {
+        if (error) {
+          console.error('Error to obtain register:', error);
+        } else {
+          room = registro.id;
+        }
+      });
+    var query= "DELETE FROM room WHERE id = ?";
+    connection.query(query, [room], (err, results)=>{
+        if(!err){
+          var query2 = "DELETE FROM project WHERE id = ?";
+            connection.query(query2,[id],(err, results)=>{
+                if(!err){
+                    return res.status(200).json({message:"room and project deleted successfully"});
+                }else{
+                    return res.status(500).json({message:"error to deleted room or project"}+err);
+                }
+            })
+        }else{
+            return res.status(500).json(err);
+        }
+    })
+})
 
 router.get('/get-rooms',auth.authenticateToken, (req,res)=>{
     let user = res.locals;
-
-    var query = "SELECT name FROM project,room WHERE project.id = room.project_id AND room.host_id= ?";
+    var query = "SELECT project.id, project.name FROM project,room WHERE project.id = room.project_id AND room.host_id= ?";
     connection.query(query, [user.id],(err, results)=>{
         if(!err){
             return res.status(200).json(results);
@@ -44,5 +69,16 @@ router.get('/get-rooms',auth.authenticateToken, (req,res)=>{
         }
     })
 })
+
+function getRoom(id, callback){
+    const query = 'SELECT * FROM room WHERE room.project_id = ?';
+    connection.query(query, [id],(error, results)=>{
+        if(error){
+            callback(error,null);
+        }else{
+            callback(null, results[0]); // Se asume que solo se espera un único registro
+        }
+    })
+}
 
 module.exports= router
